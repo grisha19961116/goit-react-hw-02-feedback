cbaf2260e285775c23024e254726177ecf5dee29
unknown <grisha1996111616@gmail.com>
2020-12-06 20:51:14 +0000

    forgot about notification

--- src/components/FeedBack/Section/Section.js
+++ src/components/FeedBack/Section/Section.js
@@ -1,9 +1,10 @@
 import style from './Section.module.css';
 export default function Section({ title, flag, children }) {
-  const titleFlag = flag ? <h1>{title}</h1> : <h3>{title}</h3>;
+  const TitleFlag = () => (flag ? <h1>{title}</h1> : <h3>{title}</h3>);
   return (
     <>
-      {titleFlag} {children}
+      <TitleFlag />
+      {children}
     </>
   );
 }
